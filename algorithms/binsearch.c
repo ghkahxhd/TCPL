28e5b23bd85ce5dc40f4aeebbd80bc3fd08fa6af
ghkahxhd <ghkahxhd@daum.net>
2018-07-24 23:28:57 +0900

    binsearch

---
+++ algorithms/binsearch.c
@@ -0,0 +1,17 @@
+/* binsearch: find x in v[0] <= v[1] <= ... <= v[n-1] */
+int binsearch(int x, int v[], int n) {
+    int low, high, mid;
+
+    low = 0;
+    high = n-1;
+    while (low <= high) {
+        mid = (low + high) / 2;
+        if (v[mid] < x)
+            high = mid-1;
+        else if (v[mid] > x)
+            low = mid+1;
+        else    /* find match */
+            return mid;
+    }
+    return -1;  /* no match */
+}
