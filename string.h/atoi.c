333cfacc15897da139f4839b8f1a229bb166c3cb
ghkahxhd <ghkahxhd@daum.net>
2018-07-24 23:35:50 +0900

    atoi_version 2

--- string.h/atoi.c
+++ string.h/atoi.c
@@ -8,3 +8,20 @@ int atoi (char s[]) {
 
     return n;
 }
+
+#include <ctype.h>
+/* atoi: convert s to integer; version 2 */
+int atoi (char s[]) {
+    int i, n, sign;
+
+    for (i = 0; isspace(s[i]); i++) /* skip white space */
+        ;
+
+    sign = (s[i] == '-')? -1 : 1;
+    if (s[i] == '-' || s[i] == '+') /* skip sign */
+        i++;
+
+    for (n = 0; isdigit(s[i]); i++)
+        n = n * 10 + (s[i] - '0');
+    return sign * n;
+}
