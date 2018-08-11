034bc9bfb2d4c706369290a974c80b19a547081d
ghkahxhd <ghkahxhd@daum.net>
2018-08-11 21:25:23 +0900

    strlen pointers

--- string.h/strlen.c
+++ string.h/strlen.c
@@ -17,3 +17,13 @@ int strlen(char *s)
         n++;
     return n;
 }
+
+/* strlen: return length of string s */
+int strlen(char *s)
+{
+    char *p = s;
+
+    while (*p != '\0')
+        p++;
+    return p - s;
+}
