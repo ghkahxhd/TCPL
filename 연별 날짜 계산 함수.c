65a248d93b05c82d434074ab977d0a6991c836e0
ghkahxhd <ghkahxhd@daum.net>
2018-08-14 21:53:44 +0900

    월 이름 출력

--- 연별 날짜 계산 함수.c
+++ 연별 날짜 계산 함수.c
@@ -25,3 +25,17 @@ void month_day(int year, int yearday, int *pmonth, int *pday)
     *pmonth = i;
     *pday = yearday;
 }
+
+/* month_name: return name of n-th month */
+char *month_name(int n)
+{
+    static char *name[] = {
+        "Illegal month",
+        "January", "February", "March",
+        "April", "May", "June",
+        "July", "August", "September",
+        "October", "November", "December"
+    };
+
+    return (n < 1 || n > 12) ? name[0] : name[n];
+}
